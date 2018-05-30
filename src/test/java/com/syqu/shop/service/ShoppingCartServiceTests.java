6a29b490d2b70c23ccbb05f0d8d12348afd09012
Syqu <aleklejawa@gmail.com>
2018-05-30 21:59:20 +0200

    Added more Cart tests

--- src/test/java/com/syqu/shop/service/ShoppingCartServiceTests.java
+++ src/test/java/com/syqu/shop/service/ShoppingCartServiceTests.java
@@ -1,13 +1,19 @@
 package com.syqu.shop.service;
 
 import com.syqu.shop.cart.ShoppingCartService;
+import com.syqu.shop.creator.ProductCreator;
+import com.syqu.shop.product.Product;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 @RunWith(SpringRunner.class)
@@ -22,5 +28,49 @@ public class ShoppingCartServiceTests {
         initMocks(this);
 
         assertThat(shoppingCartService).isNotNull();
+    }
+
+    @Test
+    public void addProductToCartTests(){
+        Map<Product, Integer> cart = new LinkedHashMap<>();
+        Product product = ProductCreator.createTestProduct();
+
+        when(shoppingCartService.productsInCart()).thenReturn(cart);
+
+        cart.put(product, 1);
+
+        assertThat(shoppingCartService.productsInCart()).containsKey(product);
+    }
+
+    @Test
+    public void addTwoTheSameProductsToCartTests(){
+        Map<Product, Integer> cart = new LinkedHashMap<>();
+        Product product = ProductCreator.createTestProduct();
+        Product product2 = ProductCreator.createTestProduct();
+
+        when(shoppingCartService.productsInCart()).thenReturn(cart);
+
+        product.setName("Not Bad Trainers");
+        product2.setName("Nice Shoes");
+
+        cart.put(product, 1);
+        cart.put(product2, 1);
+
+        assertThat(shoppingCartService.productsInCart()).containsKey(product);
+        assertThat(shoppingCartService.productsInCart()).containsKey(product2);
+    }
+
+    @Test
+    public void removeProductFromCartTests(){
+        Map<Product, Integer> cart = new LinkedHashMap<>();
+        Product product = ProductCreator.createTestProduct();
+
+        when(shoppingCartService.productsInCart()).thenReturn(cart);
+
+        cart.put(product, 1);
+        assertThat(shoppingCartService.productsInCart()).containsKey(product);
+
+        cart.remove(product);
+        assertThat(shoppingCartService.productsInCart()).doesNotContainKey(product);
     }
 }
