42844f36a3bf6012186c8aaa7619e979ffc0356b
qgrn03 <qendrim.grainca@mni.thm.de>
2021-04-19 01:22:26 +0600

    Category added

--- src/main/java/com/syqu/shop/product/Product.java
+++ src/main/java/com/syqu/shop/product/Product.java
@@ -1,5 +1,7 @@
 package com.syqu.shop.product;
 
+import com.syqu.shop.category.Category;
+import lombok.Data;
 import org.hibernate.validator.constraints.URL;
 
 import javax.persistence.*;
@@ -8,6 +10,7 @@ import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+@Data
 @Entity
 @Table(name = "product")
 public class Product {
@@ -22,6 +25,10 @@ public class Product {
     @NotEmpty
     private String name;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
     @Column(name = "description",length = 500)
     @NotNull
     @NotEmpty
@@ -34,62 +41,4 @@ public class Product {
     @NotNull
     private BigDecimal price;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImageUrl() {
-        return image_url;
-    }
-
-    public void setImageUrl(String image_url) {
-        this.image_url = image_url;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-
-        return id == product.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Id: " + getId() + " Name: " + getName() + " Description: " + getDescription() + " Price: " + getPrice();
-    }
 }
--- src/main/java/com/syqu/shop/user/User.java
+++ src/main/java/com/syqu/shop/user/User.java
@@ -1,11 +1,14 @@
 package com.syqu.shop.user;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
+@Data
 @Entity
 @Table(name = "user")
 public class User {
@@ -54,99 +57,4 @@ public class User {
     @Column(name = "balance")
     private BigDecimal balance;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Transient
-    public String getPasswordConfirm() {
-        return passwordConfirm;
-    }
-
-    public void setPasswordConfirm(String passwordConfirm) {
-        this.passwordConfirm = passwordConfirm;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
-
-    @Override
-    public String toString() {
-        return "Id: " + getId() + " User name: " + getUsername() + " E-mail: " + getEmail() + " First name: "
-                + getFirstName() + " Last name: " + getLastName() + " Age: " + getAge() + " City: " + getCity()
-                + " Gender: " + getGender() + " Balance: " + getBalance();
-    }
 }
